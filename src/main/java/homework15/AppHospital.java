2a7ee7ec3c8a0d8590b315fa37b3117a2b61edb5
Rustam <khrust_lord@mail.ru>
2022-07-25 12:45:18 -0500

    - add two objects Hospital and use arraylist from these objects

--- src/main/java/homework15/AppHospital.java
+++ src/main/java/homework15/AppHospital.java
@@ -31,10 +31,65 @@ public class AppHospital {
         myHospital.setAcceptedInsurance(franciceInsuransies);
         myHospital.setDoctors(franciceDoctors);
 
-        //myHospital.printInfoHospitalAddress();
-       // myHospital.printInsuranceCompanies();
         myHospital.printInfoHospital();
 
+        // new hospital
+
+        Hospital stDaviceHospital = new Hospital();
+        stDaviceHospital.setAddress(new Address("156 WestRoad", "Round Rock", "TX", 78566));
+        stDaviceHospital.setHospitalName("St. Davice Hospital");
+        List<Doctor> stDaviceDoctors = new ArrayList<>();
+        stDaviceDoctors.add(new Doctor("Harry", "Tomas", Position.SURGEON));
+        stDaviceDoctors.add(new Doctor("Lory", "Jordan", Position.ORTHOPEDIST));
+        stDaviceDoctors.add(new Doctor("Peter", "Horn", Position.SURGEON));
+        Map<Integer, String> stDaviceRooms = new HashMap<>();
+        stDaviceRooms.put(1,"Orthopedic");
+        stDaviceRooms.put(2, "Physiology");
+        stDaviceRooms.put(3,"SURGERY");
+        List<InsuranceCompanies> stDaviceInsurancies = new ArrayList<>();
+        stDaviceInsurancies.add(InsuranceCompanies.CIGNA);
+        stDaviceInsurancies.add(InsuranceCompanies.HCSC);
+        stDaviceHospital.setAcceptedInsurance(stDaviceInsurancies);
+        stDaviceHospital.setRooms(stDaviceRooms);
+        stDaviceHospital.setDoctors(stDaviceDoctors);
+
+        List<Hospital> arrHopitals = new ArrayList<>();
+        arrHopitals.add(stDaviceHospital);
+        arrHopitals.add(myHospital);
+        int countSurgein =0;
+        for(Hospital hospital : arrHopitals){
+            for (Doctor doc : hospital.getDoctors()){
+                System.out.println(doc.getPosition());
+                    if (doc.getPosition() == Position.SURGEON )
+                    {
+
+                        countSurgein++;
+                        System.out.println(hospital.getDoctors().listIterator().next().getPosition()  + " " + hospital.getHospitalName());
+                    }
+
+            }
+
+           }
+        System.out.println("------------------------------------------");
+        for(Hospital tmpHospital: arrHopitals){
+            tmpHospital.printInfoHospital();
 
         }
+
+        }
+
+
+
+
+//        for (Hospital hosp : arrHopitals){
+//            boolean tmp;
+//            if( hosp.getDoctors().iterator().next().getPosition()==Position.SURGEON){
+//                countSurgein ++;
+//
+//            }
+//            System.out.println(countSurgein + " " + hosp.getHospitalName());
+//        }
+
+
+//        }
 }
